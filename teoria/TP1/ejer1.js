745a1b6bfa86cc43c10dd66ea5483b1886629950
Tobias Causarano <totocausarano@gmail.com>
2026-04-27 13:55:25 -0300

    Realizando tp teoria

--- teoria/TP1/ejer1.js
+++ teoria/TP1/ejer1.js
@@ -1,3 +1,8 @@
+const readline = require('node:readline/promises');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
 function numPares (array) {
     for (let i =0; i < array.length;i++){
         if (array[i] % 2 == 0){
@@ -18,4 +23,27 @@ function repetidos (array){
     console.log(unicos)
 }
 let y = [1,1,2,3,4,4,4,5,6,6]
-repetidos(y)
+
+function orden (array){
+    ordenado = array.sort()
+    console.log(ordenado)
+}
+let o = ['c','b','a']
+
+function total (array){
+    let suma = 0
+    for (let i = 0;i<array.length;i++){
+        suma += array[i]
+    }
+    console.log(suma)
+}
+let t = [1,2,3,4,5]
+total(t)
+async function busqueda (array){
+  let  busca = await rl.question('Agregue el nombre que deseee buscar: ')
+  indice = array.indexOf(busca)
+  console.log(indice)
+
+}
+let nombres = ['Martin','Elena','Florencia']
+busqueda(nombres)
---
+++ teoria/app.js
@@ -0,0 +1,63 @@
+let nombres = []
+
+boton = document.getElementById('agregar')
+boton.addEventListener ('click',agregarNombre)
+function agregarNombre (e){
+    let inputElemento = document.getElementById('nombres')
+   let nombre = inputElemento.value
+   let punto = document.createElement('li')
+   let lista = document.getElementById('listaNombres')
+   punto.textContent = nombre
+   lista.appendChild(punto)
+   inputElemento.value = ''
+}
+function modificar (id,contenido){
+    let input = document.getElementById(id)
+    input.textContent = contenido
+}
+modificar('parrafo','hola ')
+function gestionarClase (id,clase){
+    let elemento = document.getElementById(id)
+    elemento.classList.toggle(clase)
+}
+const select = document.getElementById('edadF')
+for (let i = 0;i<50;i++){
+    let opcion = document.createElement('option')
+    opcion.textContent = i
+    select.appendChild(opcion)
+}
+function formulario (e){
+    e.preventDefault()
+    let nombreFormulario = document.getElementById('nombreF')
+    n = nombreFormulario.value
+    let apellidFormulario = document.getElementById('apellidoF')
+    a = apellidFormulario.value
+    let edadFormulario = document.getElementById('edadF')
+    e = edadFormulario.value
+    let parrafoF = document.createElement('p')
+    parrafoF.textContent = `El usuario se llama ${n} su apellido es ${a} y tiene ${e} años `
+    document.body.appendChild(parrafoF);
+}
+botonF = document.getElementById('botonF')
+botonF.addEventListener('click',formulario)
+let usuarios = [
+    { nombre: "Martin", edad: 20, foto: "https://via.placeholder.com/100" },
+    { nombre: "Guillermina", edad: 18, foto: "https://via.placeholder.com/100" },
+    { nombre: "Rosario", edad: 25, foto: "https://via.placeholder.com/100" }
+];
+const contenedor = document.getElementById('tarjetas')
+function tarjetas (usuarios){
+    usuarios.forEach(element => {
+        let div = document.createElement('div')
+        let nombreT = document.createElement('p')
+        nombreT.textContent = element.nombre
+        let edadT = document.createElement('p')
+        edadT.textContent = element.edad
+        let imagen = document.createElement('img')
+        imagen.src = elements.foto
+        div.appendChild(nombreT)
+        div.appendChild(edadT)
+        div.appendChild(imagen)
+        contenedor.appendChild(div)
+    });
+}
